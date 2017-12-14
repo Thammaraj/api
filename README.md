84af3052f46eeb575d0ec7ba0d906f44d83e8e14
thammaraj <appmsn55@gmail.com>
2017-12-14 10:32:34 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Thammaraj Slim Web API 
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require 'vendor/autoload.php';
+
+$app = new \Slim\App;
+
+$app->get('/test', function(){
+    echo 'ทดสอบ'; });
+
+$app->get('/user/register',function(){
+     echo' /user/register'; });
+
+$app->get('/category',function(){
+     echo' /category'; });
+
+$app->get('/category/product',function(){
+     echo' /category/product'; });
+
+
+
+// $app->get('/hello/{name}', function (Request $request, Response $response) {
+//     $name = $request->getAttribute('name');
+//     $response->getBody()->write("Hello, $name");
+
+//     return $response;
+// });
+$app->run();
